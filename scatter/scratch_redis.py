177d067f5e908a40221c7090591724533c73c2d7
Sankalp Sanand <sankalp@agnostiq.ai>
2024-05-14 15:23:37 +0530

    updated redis example

--- scatter/scratch_redis.py
+++ scatter/scratch_redis.py
@@ -2,5 +2,13 @@ import redis
 
 if __name__ == '__main__':
     r = redis.Redis()
-    r.set('foo', 'bar')
-    print(r.get('foo'))
+    r.set("foo", "bar")
+    val = r.get("*")
+    # print(val)
+
+    # Delete all values
+    # r.flushall()
+
+    # Get all keys
+    keys = r.keys("*")
+    print(keys)
