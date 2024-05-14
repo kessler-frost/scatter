86fc0cc87bbf5fa6ef273ad4ad368259fbb3cb83
Sankalp Sanand <sankalp@agnostiq.ai>
2024-05-14 17:02:43 +0530

    adding redis stuff

---
+++ scatter/ember/constants.py
@@ -0,0 +1,7 @@
+
+# Priority constants
+PRIO_LO = 100
+PRIO_MED = 50
+PRIO_HI = 0
+
+
--- scatter/ember/setup.py
+++ scatter/ember/setup.py
@@ -7,6 +7,25 @@ redis_broker = RedisBroker(host="127.0.0.1", port=6379, db=0)
 dramatiq.set_broker(redis_broker)
 
 
-@dramatiq.actor
-def task_execution(func_name: str, args_ref: str, kwargs_ref: str):
-    
+type_hint_actor_options = {
+    "actor_name": "type_hint_actor",
+    "queue_name": "type_hint_queue",
+    "priority": 1,
+}
+
+
+task_execution_actor_options = {
+    "actor_name": "task_execution_actor",
+    "queue_name": "task_execution_queue",
+    "priority": 2,
+}
+
+
+@dramatiq.actor(**type_hint_actor_options)
+def get_type_hints(func_name: str):
+    ...
+
+
+@dramatiq.actor(**task_execution_actor_options)
+def execute_function(func_name: str, args_ref: str, kwargs_ref: str):
+    ...
