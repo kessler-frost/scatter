48cb52539a40c6c10220671ce00f6a4331016529
Sankalp Sanand <sankalp@agnostiq.ai>
2024-05-16 21:42:14 +0530

    added showcase clients

--- scatter/client_show.py
+++ scatter/client_show.py
@@ -1,31 +1,31 @@
-from scatter.brew.core import make_callable, scatter, current_version, vaporize, rollback
-from random import randint
+from scatter.brew.core import make_callable, scatter, current_version, rollback
 
 
 if __name__ == "__main__":
 
-    num_1 = randint(0, 100)
-    num_2 = randint(0, 100)
+    num_1 = 42
+    num_2 = 42 * 24
 
-    @scatter
-    def add(a: int, b: int) -> int:
-        return a + b
+    # @scatter
+    # def add(a: int, b: int) -> int:
+    #     return a + b
 
     # @scatter
     # def add(a: int, b: int, c: int) -> int:
     #     return a + b + c
 
+    print("Current function version:", current_version("add"))
+
     callable_func = make_callable("add")
 
-    print(callable_func(num_1, num_2))
+    print("Function call result:", callable_func(num_1, num_2))
 
-    print("Current version:", current_version("add"))
+    print("Current function version:", current_version("add"))
 
-    # # Rollback the function
-    # rollback("add")
+    # Rollback the function
+    rollback("add")
+    print("Version after rollback:", current_version("add"))
 
-    # print("Version after rollback:", current_version("add"))
-
-    # # Delete the function
-    # print("Deleting the function...")
-    # vaporize("add")
+    # Calling rolledback function
+    callable_func = make_callable("add")
+    print("Rolled back function call result:", callable_func(num_1, num_2))
---
+++ scatter/client_show_3.py
@@ -0,0 +1,7 @@
+from scatter.brew.core import vaporize
+
+
+if __name__ == "__main__":
+    # Delete the function
+    print("Deleting the function...")
+    vaporize("add")
