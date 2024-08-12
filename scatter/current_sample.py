66800b23ae3c33abd1314550b2018c329359f62d
kessler-frost <sankalp@agnostiq.ai>
2024-08-12 02:57:18 -0400

    testing

--- scatter/current_sample.py
+++ scatter/current_sample.py
@@ -3,11 +3,12 @@ from scatter.main import scatter
 
 @scatter
 def sample_1(i, j):
-    return i + j * 2
+    return "i + j"
 
 
+@scatter
 def sample_2(i, j):
-    return i - j
+    return "i - j"
 
 sample_1.save()
-# save(sample_2)
+sample_2.save()
--- scatter/current_sample_2.py
+++ scatter/current_sample_2.py
@@ -7,14 +7,18 @@ def sample_3(i, j):
     return i * j
 
 
+@scatter
 def sample_4(i, j):
     return i / j
 
 
 sample_1 = assemble("sample_1")
+sample_2 = assemble("sample_2")
 
 for i in range(100):
     print(sample_1(100, 2))
+    print(sample_2(100, 2))
     time.sleep(2)
     if i % 3 == 0:
         sample_1.sync()
+        sample_2.sync()
