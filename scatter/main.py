99e96bc33b11d921888115b77d133f24aab53744
kessler-frost <sankalp@agnostiq.ai>
2024-08-11 21:29:59 -0400

    going towards jurigged based solution

--- scatter/main.py
+++ scatter/main.py
@@ -14,29 +14,23 @@ pipeline = r.pipeline()
 def save(function_: Callable) -> None:
     name: str = function_.__name__
     source: str = getsource(function_)
-    ser_func: bytes = cloudpickle.dumps(function_)
 
     # Increment/create the function version
     r.hincrby(VERSIONED_FUNCTION_HASH, name)
     version = int(r.hget(VERSIONED_FUNCTION_HASH, name))
 
-    # Save the function
+    # Save the function - using Redis Hash because for now it is simple, but it will have more fields later on
     r.hset(
         f"{name}:{version}",
         mapping={
             "source": source,
-            "function": ser_func
         }
     )
 
 
-def make_callable(name: str, pipelined: bool = False) -> Callable:
+def make_callable(name: str) -> Callable:
 
     version = int(r.hget(VERSIONED_FUNCTION_HASH, name))
-
-    if pipelined:
-        pipeline.hget(f"{name}:{version}", "function")
-        return pipeline
 
     ser_func = r.hget(f"{name}:{version}", "function")
 
--- scratch.py
+++ scratch.py
@@ -1,7 +1,7 @@
 # import inspect
 from jurigged import make_recoder
 from time import perf_counter
-import cloudpickle
+
 
 NUM = 10_00_000
 
@@ -20,23 +20,12 @@ def sample(i, j=5, k=2):
 def sample(i: int, j: float = 5.0) -> list[float]:
     return i + j
 
-
-dumped_func = cloudpickle.dumps(sample)
-
-
 print("Res:", sample(42, 42))
 
 start = perf_counter()
 for _ in range(NUM):
     sample(42, 42)
 print("Normal call:", perf_counter() - start)
-
-
-start = perf_counter()
-for _ in range(NUM):
-    cloudpickle.loads(dumped_func)(42, 42)
-print("Cloudpickle call:", perf_counter() - start)
-
 
 # Jurigged
 
